fea8be3e03a49e8c06ac1d7df15b094318f716bb
Warren Tran <warrentran2015@hotmail.com>
2022-06-06 11:25:33 -0400

    descriptive message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Excercise 1
+            for(int i = 0; i<15; i++){
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
